35691d15a201ec0764fa1d46cb7cc06b6d34b6a7
Samulimv <t3vasa03students.oamk.fi>
2024-10-12 17:00:58 +0300

    Valmis

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!, I have now enabled automatic deploys in Heroku, if it works it should be showing on my website!!!');
+    res.send('Hello, World!<br>I have now enabled automatic deploys in Heroku, if it works it should be showing on my website!!!');
 });
+
 
 app.listen(PORT, () => {
     console.log(`Palvelin toimii osoitteessa http://localhost:${PORT}`);
